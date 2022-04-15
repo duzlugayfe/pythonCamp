460b573550a6afcbcb9965f9222a5ef13eb59a67
Faruk Karaman <101044778+Faruk-Karaman@users.noreply.github.com>
2022-04-15 17:24:06 +0300

    1

--- sqliteDemo.py
+++ sqliteDemo.py
@@ -5,10 +5,8 @@ import sqlite3
 def listele():
     baglanti = sqlite3.connect("chinook/chinook.db")
     cursor = baglanti.execute("select * from customers")
-
     for satir in cursor:
         print(satir[1], satir[2])
-
     baglanti.close()
 
 
